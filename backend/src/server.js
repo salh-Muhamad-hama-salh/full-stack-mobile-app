26b2fb2322e054787ccf921539bfc3084c657b24
salh-Muhamad-hama-salh <salhmuhamadhamasalh@gmail.com>
2025-12-22 00:05:21 +0300

    Update server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
 
 const app = express();
-
-app.use(express.json());
+const __dirname = path.resolve();
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({
@@ -11,6 +12,14 @@ app.get("/api/health", (req, res) => {
   });
 });
 
-// Add your other API routes here
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-export default app;
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log(`Server is running on port ${ENV.PORT}`);
+});
