26e67c15f84c705dea0a3e54f26288bae1a4362f
salh-Muhamad-hama-salh <salhmuhamadhamasalh@gmail.com>
2025-12-22 03:00:17 +0300

    code

--- backend/src/routes/webhook.routes.js
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-// import { clerkWebhook } from "../controllers/webhook.controller.js";
-
-// const router = express.Router();
-
-// // Clerk webhook endpoint
-// router.post("/clerk", clerkWebhook);
-
-// export default router;
